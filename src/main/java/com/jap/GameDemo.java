0908ca8855d91aa20a1dbf48ccc49edc4796c51b
Sangti konyak <ssangtikonyak@gmail.com>
2022-10-16 17:08:52 +0530

    commit 1

--- src/main/java/com/jap/GameDemo.java
+++ src/main/java/com/jap/GameDemo.java
@@ -1,19 +1,23 @@
 package com.jap;
 
+import java.util.InputMismatchException;
+
 public class GameDemo {
 
     public String divideByNumber(int firstNumber, int secondNumber){
         //create local variable
-        int finalNumber = 0;
+        int sum=0;
         try {
             //write logic
-
+            sum=firstNumber/secondNumber;
             //Handle specific exception
-        }catch (Exception exception){
-          return exception.toString();
+        }catch ( ArithmeticException e){
+          return e.toString();
         }
-        return String.valueOf(finalNumber);
+        return String.valueOf(sum);
     }
-
-
+    public static void main(String[] args) {
+        GameDemo gameDemo=new GameDemo();
+        System.out.println("gameDemo.divideByNumber(10,0) = " + gameDemo.divideByNumber(10, 0));
+    }
 }
